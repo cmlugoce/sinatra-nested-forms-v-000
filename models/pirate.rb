bf4b9a740770467e7ceb5352833dcf2ada910dec
Cristina Lugo <cmlugoce@syr.edu>
2018-07-14 17:01:15 +0000

    automatically backed up by learn

---
+++ models/pirate.rb
@@ -0,0 +1,26 @@
+class Pirate 
+  
+  attr_reader :name, :weight. :height 
+  
+  @@pirates = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@pirates << self
+  end 
+  
+  def self.all 
+    @@pirates 
+  end
+  
+  def self.clear
+    @@pirates.clear
+  end
+  
+  
+  
+  
+
+end #of class 
